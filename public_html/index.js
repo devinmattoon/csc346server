51058b15b4c518592fc9c63e2ddbd254840f178b
devin mattoon <dmattoon@missouriwestern.edu>
2020-03-23 19:37:30 -0500

    State and city drop downs

---
+++ public_html/index.js
@@ -0,0 +1,43 @@
+console.log("I was here");
+
+const states = "http://localhost:8080/cities/00states.json";
+function getStates() {
+    fetch(states)
+        .then(response => {
+            return response.json();
+        })
+        .then(responseJson => {
+            console.log(responseJson);
+            viewStates(responseJson);
+            return responseJson;
+        })
+        .catch(err => console.log(err));
+}
+function viewStates(response) {
+    console.log(response[0].state);
+    let states = "";
+    for (let i = 0; i < response.length; i++) {
+        states += '<option value="' + response[i].code + '">' + response[i].state + '</option>';
+    }
+    document.getElementById("state").innerHTML = states;
+}
+function getCities(stateCode) {
+    fetch("\cities\\" + stateCode + ".json")
+        .then(response => {
+            return response.json();
+        })
+        .then(responseJson => {
+            console.log(responseJson);
+            viewCities(responseJson);
+            return responseJson;
+        })
+        .catch(err => console.log(err));
+}
+function viewCities(response) {
+    let cities = "";
+    response.forEach(function (item) {
+        cities += '<option value="' + item.lat + " " + item.long + '">' + item.city + '</option>';
+    });
+    document.getElementById("city").innerHTML = cities;
+}
+getStates();
